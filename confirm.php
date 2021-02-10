db7d7e2cc2327044097e80c9739221eae068b18e
sayaliwangane <75221058+sayaliwangane@users.noreply.github.com>
2021-02-10 22:23:26 +0530

    Add files via upload

---
+++ confirm.php
@@ -0,0 +1,37 @@
+<!-- Import data.php -->
+<?php require_once('data.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="cafe.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="order-wrapper">
+    <h2>Cart</h2>
+    <?php $totalPayment = 0 ?>
+    
+    <?php foreach ($menus as $menu): ?>
+      <?php $orderCount = $_POST[$menu->getName()] ?>
+       
+      <?php if($orderCount!=0): ?>
+        <?php 
+        $menu->setOrderCount($orderCount);
+        $totalPayment += $menu->getTotalPrice();
+        ?>
+      
+        <p class="order-amount">
+            <?php echo $menu->getName() ?>
+            x
+            <?php echo $orderCount ?>
+        </p>
+        <p class="order-price">$<?php echo $menu->getTotalPrice() ?></p>
+      <?php endif ?>
+    <?php endforeach ?>
+    <h3>Total price: $<?php echo $totalPayment ?></h3>
+  </div>
+</body>
+</html>
